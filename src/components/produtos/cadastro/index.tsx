2cf6e8932650d6ee43c4a588ad060c3ec83abc0d
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2023-03-10 17:15:39 -0300

    Criado formulario cadastro de Produtos

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -3,7 +3,39 @@ import {Layout} from 'components'
 export const CadastroProdutos: React.FC = () =>{
     return(
         <Layout titulo="Cadastro de Produtos">
-            Aqui vai ficar meu formulário
+            <div className='columns'>
+                <div className='field column is-half'>
+                    <label className='label'>Código</label>
+                    <div className='control'>
+                        <input className='input' type="text" placeholder="Código do Produto"/>
+                    </div>
+                </div>
+
+                <div className='field column is-half'>
+                    <label className='label'>Preço</label>
+                    <div className='control'>
+                        <input className='input' type="text" placeholder="Valor do Produto"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='columns'>
+                <div className='field column is-full'>
+                    <label className='label'>Nome</label>
+                    <div className='control'>
+                        <input className='input' type="text" placeholder="Nome do Produto"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className='columns'>
+                <div className='field column is-full'>
+                    <label className='label'>Descrição</label>
+                    <div className='control'>
+                    <textarea className='textarea' placeholder="Descrição do Produto"></textarea>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
